cffba3d2fa3416054c78911888744a1288bfc699
crsuk <crs-uk@outlook.com>
2017-02-20 01:26:00 +0000

    First commit of all files

---
+++ Source/QShut/MainForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace QShut
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tTitleLabel = new System.Windows.Forms.Label();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sVersionLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnCancelShutdown = new System.Windows.Forms.Button();
+            this.btnShutdown = new System.Windows.Forms.Button();
+            this.TimePicker = new System.Windows.Forms.DateTimePicker();
+            this.mainPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tTitleLabel
+            // 
+            this.tTitleLabel.AutoSize = true;
+            this.tTitleLabel.Font = new System.Drawing.Font("Segoe UI Semilight", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tTitleLabel.Location = new System.Drawing.Point(3, 3);
+            this.tTitleLabel.Name = "tTitleLabel";
+            this.tTitleLabel.Size = new System.Drawing.Size(108, 45);
+            this.tTitleLabel.TabIndex = 0;
+            this.tTitleLabel.Text = "QShut";
+            this.tTitleLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseDown);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.FlatAppearance.BorderSize = 0;
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.Font = new System.Drawing.Font("Segoe MDL2 Assets", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnExit.Location = new System.Drawing.Point(298, 7);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(40, 40);
+            this.BtnExit.TabIndex = 1;
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainPanel.Controls.Add(this.BtnExit);
+            this.mainPanel.Controls.Add(this.panel2);
+            this.mainPanel.Controls.Add(this.tTitleLabel);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(349, 239);
+            this.mainPanel.TabIndex = 2;
+            this.mainPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseDown);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.statusStrip1);
+            this.panel2.Controls.Add(this.btnCancelShutdown);
+            this.panel2.Controls.Add(this.btnShutdown);
+            this.panel2.Controls.Add(this.TimePicker);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 51);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(347, 186);
+            this.panel2.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sVersionLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 161);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(347, 25);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sVersionLabel
+            // 
+            this.sVersionLabel.Name = "sVersionLabel";
+            this.sVersionLabel.Size = new System.Drawing.Size(83, 20);
+            this.sVersionLabel.Text = "QShut 1.0.1";
+            // 
+            // btnCancelShutdown
+            // 
+            this.btnCancelShutdown.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.btnCancelShutdown.Location = new System.Drawing.Point(10, 109);
+            this.btnCancelShutdown.Name = "btnCancelShutdown";
+            this.btnCancelShutdown.Size = new System.Drawing.Size(328, 44);
+            this.btnCancelShutdown.TabIndex = 2;
+            this.btnCancelShutdown.Text = "Cancel Shutdown";
+            this.btnCancelShutdown.UseVisualStyleBackColor = true;
+            this.btnCancelShutdown.Click += new System.EventHandler(this.btnCancelShutdown_Click);
+            // 
+            // btnShutdown
+            // 
+            this.btnShutdown.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShutdown.Location = new System.Drawing.Point(10, 63);
+            this.btnShutdown.Name = "btnShutdown";
+            this.btnShutdown.Size = new System.Drawing.Size(328, 43);
+            this.btnShutdown.TabIndex = 1;
+            this.btnShutdown.Text = "Submit";
+            this.btnShutdown.UseVisualStyleBackColor = true;
+            this.btnShutdown.Click += new System.EventHandler(this.BtnShutdown_Click);
+            // 
+            // TimePicker
+            // 
+            this.TimePicker.CalendarFont = new System.Drawing.Font("Roboto", 16F);
+            this.TimePicker.CalendarMonthBackground = System.Drawing.SystemColors.Menu;
+            this.TimePicker.Font = new System.Drawing.Font("Segoe UI Semilight", 16F);
+            this.TimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.TimePicker.Location = new System.Drawing.Point(10, 14);
+            this.TimePicker.Name = "TimePicker";
+            this.TimePicker.Size = new System.Drawing.Size(328, 43);
+            this.TimePicker.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 239);
+            this.Controls.Add(this.mainPanel);
+            this.Font = new System.Drawing.Font("Roboto", 11F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tTitleLabel;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DateTimePicker TimePicker;
+        private System.Windows.Forms.Button btnCancelShutdown;
+        private System.Windows.Forms.Button btnShutdown;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel sVersionLabel;
+    }
+}
+
---
+++ Source/QShut/MainForm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace QShut
+{
+
+    /// <summary>
+    /// A simple application to schedule the PC to shut down in 'x' seconds.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd,
+            int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Segoe UI MDL2 close button icon
+            BtnExit.Text = "\xE106";
+        }
+
+        private void BtnExit_Click(object sender, System.EventArgs e)
+        {
+            // Exit the application
+            Application.Exit();
+        }
+
+        private void BtnShutdown_Click(object sender, System.EventArgs e)
+        {
+            // String passed to the console to commit the full shutdown command
+            string strShutDown = @"shutdown -s -t " + TimeDifference();
+
+            Process cmd = new Process();
+            ProcessStartInfo cmdInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,    // Hide the console on start
+                FileName = "cmd.exe",                       // Start the console
+                Arguments = "/c " + strShutDown             // Pass the shutdown string
+            };
+
+            cmd.StartInfo = cmdInfo;                        // Pass the cmd process its start info
+            cmd.Start();                                    // Start the cmd process
+        }
+
+        private void btnCancelShutdown_Click(object sender, EventArgs e)
+        {
+            // Same as above, but instead we pass the console 'shutdown -a' to cancel any pending shutdown requests
+            Process cmd = new Process();
+            ProcessStartInfo cmdInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "cmd.exe",
+                Arguments = "/c shutdown -a"
+            };
+
+            cmd.StartInfo = cmdInfo;
+            cmd.Start();
+        }
+
+        public int TimeDifference()
+        { 
+            DateTime usrSelectedTime = TimePicker.Value;        // Retrieve the time the user has selected
+            DateTime timeNow = DateTime.Now.ToLocalTime();      // Retrieve the current PC time
+            double diffTimeInSeconds = (usrSelectedTime - timeNow).TotalSeconds;    // Calculate the difference between now and the time the user has selected
+            int timeInInt = Convert.ToInt32(diffTimeInSeconds); // Convert the difference to int
+
+            // If the user has selected a time before the current time, inform the user of their mistake
+            if (usrSelectedTime < timeNow)
+            {
+                MessageBox.Show(@"You cannot pick a time earlier than the current time", "Error!", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+            return timeInInt;
+        }
+
+        private void mainPanel_MouseDown(object sender, MouseEventArgs e)
+        {   
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
